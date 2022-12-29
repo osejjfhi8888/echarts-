8e54912fa9ec9f371080f75742a0709d4bec02fd
厉害o.0 <111895951+osejjfhi8888@users.noreply.github.com>
2022-12-29 16:20:36 +0800

    Add files via upload
    提交2.0

---
+++ src/views/SellerPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="com-page">
+    <seller></seller>
+  </div>
+</template>
+
+<script>
+import Seller from '@/components/Seller'
+export default {
+  components: {
+    seller: Seller
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
